e808f36e47e800b421e8b8327f0536d0fc46cc27
sanjaymagarzph <sanjay.magar@zakipoint.com>
2020-08-29 02:15:37 +0545

    :wrench:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Gauge from './components';
+
+const value = 70,
+  min = 0,
+  max = 100,
+  label = 'Label',
+  units = 'unit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Gauge {...{ value, min, max, label, units }} />
     </div>
   );
 }
